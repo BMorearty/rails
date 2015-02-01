6cb731feda474cbe54313759229a000e3440223f
Brian Morearty <brian@morearty.org>
2015-01-31 18:39:56 -0800

    Make the tests better.
    Now, they check which ids were updated.
    
    Have to sort the ids when touching in order to test them against a regex.

--- activerecord/test/cases/delay_touching_test.rb
+++ activerecord/test/cases/delay_touching_test.rb
@@ -12,7 +12,7 @@ class DelayTouchingTest < ActiveRecord::TestCase
   end
 
   test "delay_touching consolidates touches on a single record" do
-    expect_updates ["owners"] do
+    expect_updates [ { "owners" => { ids: owner } } ] do
       ActiveRecord::Base.delay_touching do
         owner.touch
         owner.touch
@@ -47,7 +47,7 @@ class DelayTouchingTest < ActiveRecord::TestCase
   end
 
   test "delay_touching consolidates touches for all instances in a single table" do
-    expect_updates ["pets", "owners"] do
+    expect_updates [ { "pets" => { ids: [ pet1, pet2 ] } }, "owners" => { ids: owner } ] do
       ActiveRecord::Base.delay_touching do
         pet1.touch
         pet2.touch
@@ -66,7 +66,7 @@ class DelayTouchingTest < ActiveRecord::TestCase
   end
 
   test "delay_touching only applies touches for which no_touching is off" do
-    expect_updates ["pets"] do
+    expect_updates [ "pets" => { ids: pet1 } ] do
       Owner.no_touching do
         ActiveRecord::Base.delay_touching do
           owner.touch
@@ -77,7 +77,7 @@ class DelayTouchingTest < ActiveRecord::TestCase
   end
 
   test "delay_touching does not apply nested touches if no_touching was turned on inside delay_touching" do
-    expect_updates [ "owners" ] do
+    expect_updates [ "owners" => { ids: owner } ] do
       ActiveRecord::Base.delay_touching do
         owner.touch
         ActiveRecord::Base.no_touching do
@@ -88,7 +88,7 @@ class DelayTouchingTest < ActiveRecord::TestCase
   end
 
   test "delay_touching can update nonstandard columns" do
-    expect_updates [ "owners" => [ "updated_at", "happy_at" ] ] do
+    expect_updates [ "owners" => { ids: owner, columns: ["updated_at", "happy_at"] } ] do
       ActiveRecord::Base.delay_touching do
         owner.touch :happy_at
       end
@@ -96,7 +96,10 @@ class DelayTouchingTest < ActiveRecord::TestCase
   end
 
   test "delay_touching splits up nonstandard column touches and standard column touches" do
-    expect_updates [ { "pets" => [ "updated_at", "neutered_at" ]  }, { "pets" => [ "updated_at" ] }, "owners" ] do
+    expect_updates [ { "pets" => { ids: [ pet1 ], columns: [ "updated_at", "neutered_at" ] } },
+                     { "pets" => { ids: [ pet2 ], columns: [ "updated_at" ] } },
+                       "owners" ] do
+
       ActiveRecord::Base.delay_touching do
         pet1.touch :neutered_at
         pet2.touch
@@ -105,7 +108,9 @@ class DelayTouchingTest < ActiveRecord::TestCase
   end
 
   test "delay_touching can update multiple nonstandard columns of a single record in different calls to touch" do
-    expect_updates [ { "owners" => [ "updated_at", "happy_at" ] }, { "owners" => [ "updated_at", "sad_at" ] } ] do
+    expect_updates [ { "owners" => { ids: owner, columns: [ "updated_at", "happy_at" ] } },
+                     { "owners" => { ids: owner, columns: [ "updated_at", "sad_at" ] } } ] do
+
       ActiveRecord::Base.delay_touching do
         owner.touch :happy_at
         owner.touch :sad_at
@@ -113,8 +118,18 @@ class DelayTouchingTest < ActiveRecord::TestCase
     end
   end
 
+  test "delay_touching can update multiple nonstandard columns of a single record in a single call to touch" do
+    expect_updates [ { "owners" => { ids: owner, columns: [ "updated_at", "happy_at" ] } },
+                     { "owners" => { ids: owner, columns: [ "updated_at", "sad_at" ] } } ] do
+
+      ActiveRecord::Base.delay_touching do
+        owner.touch :happy_at, :sad_at
+      end
+    end
+  end
+
   test "delay_touching consolidates touch: true touches" do
-    expect_updates [ "pets", "owners" ] do
+    expect_updates [ { "pets" => { ids: [ pet1, pet2 ] } }, { "owners" => { ids: owner } } ] do
       ActiveRecord::Base.delay_touching do
         pet1.touch
         pet2.touch
@@ -123,11 +138,27 @@ class DelayTouchingTest < ActiveRecord::TestCase
   end
 
   test "delay_touching does not touch the owning record via touch: true if it was already touched explicitly" do
-    expect_updates [ "pets", "owners" ] do
+    expect_updates [ { "pets" => { ids: [ pet1, pet2 ] } }, { "owners" => { ids: owner } } ] do
       ActiveRecord::Base.delay_touching do
         owner.touch
         pet1.touch
         pet2.touch
+      end
+    end
+  end
+
+  test "delay_touching consolidates touch: :column_name touches" do
+    klass = Class.new(ActiveRecord::Base) do
+      def self.name; 'Pet'; end
+      belongs_to :owner, :touch => :happy_at
+    end
+
+    pet = klass.first
+
+    expect_updates [ { "owners" => { ids: owner, columns: [ "updated_at", "happy_at" ] } }, { "pets" => { ids: pet } } ] do
+      ActiveRecord::Base.delay_touching do
+        pet.touch
+        pet.touch
       end
     end
   end
@@ -146,9 +177,9 @@ class DelayTouchingTest < ActiveRecord::TestCase
     @pet2 ||= owner.pets.last
   end
 
-  def expect_updates(tables_and_columns)
+  def expect_updates(tables_ids_and_columns)
     capture_sql { yield }
-    expected_sql = expected_sql_for(tables_and_columns)
+    expected_sql = expected_sql_for(tables_ids_and_columns)
     ActiveRecord::SQLCounter.log.each do |stmt|
       if stmt =~ /UPDATE /i
         index = expected_sql.index { |expected_stmt| stmt =~ expected_stmt }
@@ -159,16 +190,23 @@ class DelayTouchingTest < ActiveRecord::TestCase
     assert_empty expected_sql, "Some of the expected updates were not executed"
   end
 
-  def expected_sql_for(tables_and_columns)
-    tables_and_columns.map do |entry|
+  def expected_sql_for(tables_ids_and_columns)
+    tables_ids_and_columns.map do |entry|
       if entry.kind_of?(Hash)
-        entry.map do |table, columns|
-          Regexp.new(%{UPDATE "#{table}" SET #{columns.map { |column| %{"#{column}" =.+} }.join(", ") } })
+        entry.map do |table, options|
+          ids = Array.wrap(options[:ids])
+          columns = Array.wrap(options[:columns]).presence || ["updated_at"]
+          Regexp.new(%{UPDATE "#{table}" SET #{columns.map { |column| %{"#{column}" =.+} }.join(", ") } .+#{ids_sql(ids)}\\Z})
         end
       else
         Regexp.new(%{UPDATE "#{entry}" SET "updated_at" = })
       end
     end.flatten
+  end
+
+  def ids_sql(ids)
+    ids = ids.map { |id| id.class.respond_to?(:primary_key) ? id.send(id.class.primary_key) : id }
+    ids.length > 1 ? %{ IN \\(#{ids.sort.join(", ")}\\)} : %{ = #{ids.first}}
   end
 end
 
